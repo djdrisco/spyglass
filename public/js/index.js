a56c66aa38941d3f9af9fe3f3af257abbc5b67a1
cjfont <cjfont@gmail.com>
2015-05-06 16:45:36 -0400

    Initial working report: External IPs

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,48 @@
 $(function() {
     
   $('#external_ips').click(function() {
-    console.log("test");
+    
+    $("#results").text("");
+    $("#results").append($("<img>").attr("src", baseUrl+"/img/loader.gif"));
+    
+    $.ajax({
+      url: baseUrl + "/external_ips",
+      success: function(data) {
+        var result_table = _.flatten(_.map(data, function(region) {
+          return _.map(region.Addresses, function(addr) {
+            return [addr.PublicIp, addr.PrivateIpAddress, addr.Domain, region.Region]
+          });
+        }));
+        result_table.unshift(['Public IP', 'Private IP', 'Domain', 'Region'])
+        create_table(result_table);
+      },
+      dataType: "json"
+    })
   });
   
 });
+
+function create_table(table_data) {
+
+  var table = $("<table>").attr("class", "results");
+  var tr = $("<tr>");
+  _.each(_.first(table_data), function(field_name) {
+    tr.append($("<th>").text(field_name));
+  });
+  table.append($("<thead>").append(tr));
+  
+  var tbody = $("<tbody>");
+
+  _.each(_.rest(table_data), function(row) {
+    var tr = $("<tr>");
+    _.each(row, function(field) {
+      tr.append($("<td>").text(field));
+    });
+    tbody.append(tr);
+  });
+  
+  table.append(tbody);
+  
+  $("#results").text("");
+  $("#results").append(table);
+} 
--- routes/reports.js
+++ routes/reports.js
@@ -1,8 +1,12 @@
+var async = require('async');
 var AWS = require('aws-sdk');
 var express = require('express');
 var proxy = require('proxy-agent');
 var https_proxy = require("https-proxy-agent");
 var router = express.Router();
+
+var apiVersion = '2014-02-01';
+var regions = ['us-east-1', 'us-west-1'];
 
 if (process.env.HTTP_PROXY) {
   AWS.config.update({
@@ -10,18 +14,25 @@ if (process.env.HTTP_PROXY) {
   });
 }
 
-var ec2 = new AWS.EC2({
-  region: 'us-east-1', 
-  apiVersion: "2014-02-01"
-});
-
 router.get('/external_ips', function(req, res) {
-  console.log("---");
-  ec2.describeInstances({}, function(err, data) {
+  collect_regions("describeAddresses", {}, function(err, data) {
     if (err) console.error(err);
-    console.log(">>>", data);
     res.send(data);
   });
 });
 
 module.exports = router;
+
+function collect_regions(action, params, callback) {
+  async.map(regions, function(region, cb) {
+    var ec2 = new AWS.EC2({
+      region: region,
+      apiVersion: apiVersion
+    });
+    ec2[action](params, function(err, data) {
+      if (err) return cb(err);
+      data.Region = region;
+      cb(null, data);
+    });
+  }, callback);  
+}
